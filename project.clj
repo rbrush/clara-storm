97fa68df71c5791a4dc61f3aa831c5e431524692
Ryan Brush <rbrush@cerner.com>
2013-11-03 14:45:05 -0600

    Refactor project to prepare for Java API.

--- project.clj
+++ project.clj
@@ -1,8 +1,14 @@
-(defproject clara/clara-storm "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject org.toomuchcode/clara-storm "0.1.0-SNAPSHOT"
+  :description "Clara Rules -- Storm Support"
+  :url "http://rbrush.github.io/clara-storm/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.toomuchcode/clara-rules "0.3.0-SNAPSHOT"]
-                 [storm "0.8.2"]])
+                 [storm "0.8.2"]]
+  :plugins [[codox "0.6.4"]
+            [lein-javadoc "0.1.1"]]
+  :javadoc-opts {:package-names ["clara.storm"]}
+  :source-paths ["src/main/clojure"]
+  :test-paths ["src/test/clojure"]
+  :java-source-paths ["src/main/java"])
