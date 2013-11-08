ccb0ab2965ff09fba926fc283df2f3db6a85f92b
Ryan Brush <rbrush@cerner.com>
2013-11-08 16:23:11 -0600

    Add non-RPC attachment to topology and JavaDoc.

--- src/main/java/clara/storm/RuleBolts.java
+++ src/main/java/clara/storm/RuleBolts.java
@@ -29,6 +29,16 @@ public class RuleBolts {
         attachTopology = RT.var("clara.rules.storm-java", "attach-topology");
     }
 
+    /** 
+     * Attach a set of rules to a topology. This is primary for local testing with the given RPC client.
+     * 
+     * @param builder the topology builder to attach the rules to the rules session.
+     * @param drpc A distributed RPC client used to query
+     * @param factSourceIds the identifiers of spouts or bolts that produce items on the "facts" stream used by the rule engine.
+     * @param querySourceId the identifier of the DRPC used for issuing queries to the topology
+     * @param rulesets One or more Clojure namespaces containing Clara rules. The namespaces must be visible in the caller's classloader
+     * @return a client for issuing queries to the rule engine.
+     */
     public static QueryClient attach(TopologyBuilder builder,
                                      LocalDRPC drpc,
                                      List<String> factSourceIds,
@@ -36,5 +46,20 @@ public class RuleBolts {
                                      String... rulesets) {
 
         return (QueryClient) attachTopology.invoke(builder, drpc, factSourceIds, querySourceId, Arrays.asList(rulesets));
+    }
+
+    /** 
+     * Attach a set of rules to a topology, without using a query client.
+     * 
+     * @param builder the topology builder to attach the rules to
+     * @param factSourceIds the identifiers of spouts or bolts that produce items on the "facts" stream used by the rule engine.
+     * @param querySourceId the identifier of the DRPC used for issuing queries to the topology
+     * @param rulesets One or more Clojure namespaces containing Clara rules. The namespaces must be visible in the caller's classloader
+     */
+    public static void attach(TopologyBuilder builder,
+                              List<String> factSourceIds,
+                              String... rulesets) {
+
+        attachTopology.invoke(builder, null, factSourceIds, null, Arrays.asList(rulesets));
     }
 }
